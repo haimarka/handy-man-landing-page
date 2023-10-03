0969f1f4111d28c859fca0a811ac79dc435fea14
haim arka <87427194+haimarka@users.noreply.github.com>
2023-10-03 12:44:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+// header slider
+
+const swiper = new Swiper('.swiper', {
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+  // smooth slide
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e){
+        e.preventDefault(); 
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+// elements slide
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true
+});
+
+sr.reveal(`.slider-container, .banner, .footer`, {
+    origin: 'top',
+    interval: 150
+})
+
+sr.reveal(`.header__container, .prices__container`, {
+    origin: 'left'
+})
+
+sr.reveal(`.about__container, .display__container`, {
+    origin: 'right'
+})
+
+
+  // hide form
+
+function showForm(){
+    let form = document.getElementById("form")
+    if(form.style.display === 'none'){
+        form.style.display = "block";
+    }else{
+        form.style.display = "none";
+    }
+}
